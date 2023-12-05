14f5643e49d789c2878ab7350708829696f739e6
Olanakelbesa <130336147+Olanakelbesa@users.noreply.github.com>
2023-12-05 12:40:38 +0300

    Added solution for Print Words Vertically

---
+++ ongoing/print-words-vertically.py
@@ -0,0 +1,22 @@
+class Solution:
+    def printVertically(self, s):
+        split = s.split()
+        
+        result = []
+        
+        maxWordLength = max(len(word) for word in split)
+        
+        for indexInWord in range(maxWordLength):
+            partialResult = []
+            for word in split:
+                if len(word) > indexInWord:
+                    partialResult.append(word[indexInWord])
+                else:
+                    partialResult.append(" ")
+            result.append(partialResult)
+        
+        output = []
+        for partialResult in result:
+            output.append(''.join(partialResult).rstrip())
+            
+        return output
