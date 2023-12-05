48303f9f34a65eb4f97e1654388c12a8c6b90a61
Olanakelbesa <130336147+Olanakelbesa@users.noreply.github.com>
2023-12-05 18:33:55 +0300

    Added solution for Multiply Strings

---
+++ ongoing/multiply-strings.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        number1 = int(num1)
+        number2 = int(num2)
+        product = number1 * number2
+        return str(product)
+        
