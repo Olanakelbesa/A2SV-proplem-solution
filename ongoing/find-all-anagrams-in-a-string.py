7b93aa3ac341bd054115a47aec3a6215ffe54519
Olanakelbesa <130336147+Olanakelbesa@users.noreply.github.com>
2024-01-10 20:21:59 +0300

    Added solution for Find All Anagrams in a String

--- ongoing/find-all-anagrams-in-a-string.py
+++ ongoing/find-all-anagrams-in-a-string.py
@@ -1,11 +1,19 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        k = len(p)
         target = (Counter(p))
-        ans=[]
-        for i in range( len(s)):
-            target[s[i]]-=1
-            if i>=len(p):target[s[i-len(p)]]+=1
-            if all(target[j]==0 for j in target):ans.append(i-len(p)+1)
-        # print(arr)
-        return ans
+        window = (Counter(s[:k]))
+        arr = []
+        for i in range(k, len(s)):
+            if target == window:
+                arr.append(i-k)
+            window[s[i-k]] -= 1
+            if window[s[i-k]] == 0:
+                del window[s[i-k]]
+            window[s[i]] += 1
+        if target == window:
+            arr.append(len(s) - k)
+            
+        print(arr)
+        return arr
 
