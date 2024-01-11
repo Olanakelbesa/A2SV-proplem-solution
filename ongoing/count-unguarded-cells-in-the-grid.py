142bd07530e5162ba0d4b7d04fad2762282f19e0
Olanakelbesa <130336147+Olanakelbesa@users.noreply.github.com>
2024-01-11 15:15:06 +0300

    Added solution for Count Unguarded Cells in the Grid

--- ongoing/count-unguarded-cells-in-the-grid.py
+++ ongoing/count-unguarded-cells-in-the-grid.py
@@ -1,72 +1,57 @@
 class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
-      grid = [[0]*n for _ in range(m)]
-      for x, y in guards:
-        grid[x][y] = 'G'
-      for x, y in walls:
-        grid[x][y] = 'W'
-        
-      grid2 = [[0]*n for _ in range(m)]
-      for x, y in guards:
-        grid2[x][y] = 'G'
-      for x, y in walls:
-        grid2[x][y] = 'W'
-        
-      for i in range(m):
-        cache = []
-        has_G = False
-        for j in range(n):
-          if grid[i][j] == 0:
-            cache.append(j)
-          elif grid[i][j] == 'G':
-            if cache:
-              for k in cache:
-                grid[i][k] = 'O'
-              cache = []
-            has_G = True
-          else:
-            if has_G:
-              if cache:
-                for k in cache:
-                  grid[i][k] = 'O'
-                  cache = []
-              has_G = False
-            else:
-              cache = []
-        if cache:
-          if has_G:
-            for k in cache:
-                grid[i][k] = 'O'
-      
-      for j in range(n):
-        cache = []
-        has_G = False
-        for i in range(m):
-          if grid2[i][j] == 0:
-            cache.append(i)
-          elif grid2[i][j] == 'G':
-            if cache:
-              for k in cache:
-                grid2[k][j] = 'O'
-              cache = []
-            has_G = True
-          else:
-            if has_G:
-              if cache:
-                for k in cache:
-                  grid2[k][j] = 'O'
-                  cache = []
-              has_G = False
-            else:
-              cache = []
-        if cache:
-          if has_G:
-            for k in cache:
-                grid2[k][j] = 'O'
-      
-      res = 0
-      for i in range(m):
-        for j in range(n):
-          if grid[i][j] == 0 and grid2[i][j] == 0: res += 1
-      return res
-        
+    	grid=[[1 for j in range(n)] for i in range(m)]
+    	# 2->walls
+    	# 3->guards
+    	for i,j in walls:
+    		grid[i][j]=2
+    	for i,j in guards:
+    		grid[i][j]=3
+
+    	for i in range(m):
+    		safe=[]
+    		guarded=[]
+    		for j in range(n):
+    			if grid[i][j]==3:
+    				while safe:
+    					x,y=safe.pop()
+    					grid[x][y]=0
+    				guarded.append((i,j))
+    			elif grid[i][j]==2:
+    				while guarded:
+    					guarded.pop()
+    				while safe:
+    					safe.pop()
+    			elif grid[i][j]==1:
+    				if guarded:
+    					grid[i][j]=0
+    				else:
+    					safe.append((i,j))
+
+    	for j in range(n):
+    		safe=[]
+    		guarded=[]
+    		for i in range(m):
+    			if grid[i][j]==3:
+    				while safe:
+    					x,y=safe.pop()
+    					grid[x][y]=0
+    				guarded.append((i,j))
+    			elif grid[i][j]==2:
+    				while guarded:
+    					guarded.pop()
+    				while safe:
+    					safe.pop()
+    			elif grid[i][j]==1:
+    				if guarded:
+    					grid[i][j]=0
+    				else:
+    					safe.append((i,j))
+
+    	res=0
+    	for i in range(m):
+    		for j in range(n):
+    			if grid[i][j]==1:
+    				res+=1
+
+    	return res
